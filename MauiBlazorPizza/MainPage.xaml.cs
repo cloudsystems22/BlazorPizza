b75059efc41a433fd77277eb5c499b7847c83753
cloudsystems22 <cloudsystems22@gmail.com>
2023-12-14 17:52:40 -0300

    first commit

---
+++ MauiBlazorPizza/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorPizza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiBlazorPizza/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBlazorPizza
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
