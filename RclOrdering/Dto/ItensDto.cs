030f12b8f83dd9563f169a6ffd1ac0b374b91434
cloudsystems22 <cloudsystems22@gmail.com>
2023-12-28 15:41:20 -0300

    componentes pedido e tipo de entrega

---
+++ RclOrdering/Dto/ItensDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RclOrdering.Dto
+{
+    public class ItensDto
+    {
+        public string? Uid { get; set; }
+
+        public string? Item { get; set; }
+
+        public string? Size { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float Value { get; set; }
+    }
+}
---
+++ RclOrdering/Dto/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿namespace RclOrdering.Dto
+{
+    public class OrderDto
+    {
+        public string? Uid { get; set; }
+
+        public string? UidUser { get; set; }
+
+        public DateTime OrderData { get; set; }
+
+        public List<ItensDto>? Itens { get; set; }
+
+        public float Tax { get; set; }
+
+        public float Amount { get; set; }
+
+
+    }
+}
